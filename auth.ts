ff6d1b0b73b22dbb3e4df81d27929e6ddc3ceb9e
NuttasitNew <beginnew@hotmail.com>
2024-05-20 13:27:20 +0700

    [file] auth.js [desc] fix user.id with check condition it has user or not

--- auth.ts
+++ auth.ts
@@ -13,16 +13,20 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   },
   events: {
     async linkAccount({ user }) {
-      await db.user.update({
-        where: { id: user.id },
-        data: { emailVerified: new Date() },
-      });
+      if (user.id) {
+        await db.user.update({
+          where: { id: user.id },
+          data: { emailVerified: new Date() },
+        });
+      }
     },
   },
   callbacks: {
     async signIn({ user, account }) {
       // Allow OAuth without email verfication
       if (account?.provider !== "credentials") return true;
+
+      if (!user.id) return false;
 
       const existingUser = await getUserById(user.id);
 
