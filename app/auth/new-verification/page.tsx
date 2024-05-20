9fac50298b3f7a1dd835c8f62f1e2266a5465419
NuttasitNew <beginnew@hotmail.com>
2024-05-20 14:07:33 +0700

    [file] auth/new-verification/page.tsx [desc] fix import

--- app/auth/new-verification/page.tsx
+++ app/auth/new-verification/page.tsx
@@ -1,4 +1,4 @@
-import NewVerificationForm from "@/components/auth/new-verification-form";
+import { NewVerificationForm } from "@/components/auth/new-verification-form";
 import React from "react";
 
 const NewVerificationPage = () => {
